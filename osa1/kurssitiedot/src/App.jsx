ecbd61d694f0b209b9215f81e9949ef8ecee22e6
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2025-12-20 15:33:41 +0200

    Refactored kurssitiedot (step1)

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,43 @@
+const Header = ({ course }) => {
+  return <h1>{course}</h1>
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <p>
+        {parts[0].name} {parts[0].exercises}
+      </p>
+      <p>
+        {parts[1].name} {parts[1].exercises}
+      </p>
+      <p>
+        {parts[2].name} {parts[2].exercises}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
+
+const Total = ({ parts }) => {
+  const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return <p>Number of exercises {sum}</p>
+}
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+
+export default App
