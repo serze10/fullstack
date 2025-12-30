5460d35937145b4882b2476d540d371575a2fd53
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2025-12-30 18:56:19 +0200

    osa1c

--- osa1/part1/src/App.tsx
+++ osa1/part1/src/App.tsx
@@ -1,9 +1,31 @@
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
-  const friends = [ 'Leevi', 'Venla']
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <p>{friends}</p>
+      <Display counter={counter}/>
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
 }
