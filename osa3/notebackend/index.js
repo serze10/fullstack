1792f80cc96bff3375890cd6266de9c1aecb9500
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2026-01-08 19:21:31 +0200

    3.7-3.8

--- osa3/notebackend/index.js
+++ osa3/notebackend/index.js
@@ -21,6 +21,14 @@ let notes = [
   }
 ]
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -77,3 +85,9 @@ const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
--- osa3/puhelinluettelonbackend/index.js
+++ osa3/puhelinluettelonbackend/index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (request, response) => {
+  return JSON.stringify(request.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     {
@@ -81,6 +88,7 @@ app.post('/api/persons', (request, response) => {
   
   response.json(person)
 })
+
 
 const PORT = 3001
 app.listen(PORT, () => {
