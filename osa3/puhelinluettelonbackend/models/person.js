2f8ea0326202f5445b69c052ca1ec28bad7b078a
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2026-01-10 20:26:37 +0200

    Add phone number validation and frontend build

--- osa3/puhelinluettelonbackend/models/person.js
+++ osa3/puhelinluettelonbackend/models/person.js
@@ -14,8 +14,22 @@ mongoose.connect(url, { family: 4 })
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number. Number must be in format XX-XXXX or XXX-XXXX with at least 8 characters total.`
+    }
+  }
 })
 
 personSchema.set('toJSON', {
