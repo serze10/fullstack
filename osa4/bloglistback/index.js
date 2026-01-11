730cd5f5e5bc7bf61fcd4eae4f4318b6cc68c422
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2026-01-11 15:02:52 +0200

    4.2

--- osa4/bloglistback/index.js
+++ osa4/bloglistback/index.js
@@ -1,35 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+const app = require('./app')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const app = express()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bloglist'
-mongoose.connect(MONGODB_URI, { family: 4 })
-  .then(() => console.log('connected to MongoDB'))
-  .catch(err => console.error('error connecting to MongoDB:', err.message))
-
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => response.json(blogs))
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  blog.save().then(result => response.status(201).json(result))
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
