96029dd29018a95d9397bf26e7b074a319d64cad
Santeri Silvennoinen <santeri.silvennoinen@gmail.com>
2026-03-25 19:50:27 +0200

    4.12

--- osa4/bloglistback/tests/blog_api.test.js
+++ osa4/bloglistback/tests/blog_api.test.js
@@ -70,6 +70,38 @@ test('if likes is missing, it defaults to 0', async () => {
   assert.strictEqual(response.body.likes, 0)
 })
 
+test('blog without title is not added', async () => {
+  const newBlog = {
+    author: 'Missing Title',
+    url: 'https://missing-title.dev',
+    likes: 1,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
+test('blog without url is not added', async () => {
+  const newBlog = {
+    title: 'Missing URL',
+    author: 'Missing URL Author',
+    likes: 1,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(400)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
